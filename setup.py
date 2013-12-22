29faf19561010b8d838b4c30b975989a34e4bc06
Arie Bro <ariebro@gmail.com>
2013-12-22 13:50:18 +0200

    Fixed packaging issues

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import os
-from setuptools import setup
-VERSION = "1.0"
+from setuptools import setup, find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def read(fname):
+def fread(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 def get_version():
+    VERSION = fread("VERSION").strip()
     build_number = os.environ.get('BUILD_NUMBER', None)
     if build_number:
         return VERSION + "b{}".format(build_number)
@@ -23,15 +23,13 @@ setup(
     description = "A small package that helps generate content to fill databases for tests.",
     url = "http://github.com/arieb/python-testdata",
     license = "MIT",
-    package_data = {
-        '': ['.md']
-    },
     install_requires = [
         'fake-factory == 0.2'
     ],
     keywords = "factory testing test unittest mongo data testdata database json elasticsearch",
-    packages = ['testdata', 'testdata.extra', 'testdata.factories'],
-    long_description=read('README.md'),
+    packages = find_packages(),
+    include_package_data=True,
+    long_description=fread('README.md'),
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Testing",
